e8b967ab9c801de1fd726275c2d17e3ac6db8a4e
Kent Quirk <kentquirk@honeycomb.io>
2023-12-26 23:44:58 -0500

    colors and details

--- p5/probtoy/sketch.js
+++ p5/probtoy/sketch.js
@@ -5,16 +5,18 @@ let balls;
 
 let width = 600;
 let height = 780;
-let ballcount = 350;
+let ballcount = 360;
 let ballsize = 8;
+let ballcolors = ['#ff0000', '#ffff00', '#00ff00'];
+let ballcolorindex = 0;
 let pinsize = 3;
 let pinmax = 35;
 let pinrows = 17;
 let pinstart = pinmax - pinrows - 1;
-let pintop = 170;
+let pintop = 160;
 let wallthick = 10;
 let ndividers = pinmax;
-let divheight = 300;
+let divheight = 320;
 let divthick = 7;
 
 function equalSpacing(n, divwid) {
@@ -33,19 +35,19 @@ function equalSpacing(n, divwid) {
 
 function setup() {
 	new Canvas(width, height);
-  world.gravity.y = 8;
+  world.gravity.y = 9;
 
   funnel = new Group();
-  funnel.color = 'blue';
+  funnel.color = 'black';
   funnel.collider = 'static';
 
-  let lf = new funnel.Sprite(width/4-8, height/8, width/2, wallthick);
+  let lf = new funnel.Sprite(width/4-5, height/8, width/2, wallthick*2);
   lf.rotation = 20;
-  let rf = new funnel.Sprite(3*width/4+8, height/8, width/2, wallthick);
+  let rf = new funnel.Sprite(3*width/4+5, height/8, width/2, wallthick*2);
   rf.rotation = -20;
 
   box = new Group();
-  box.color = 'blue';
+  box.color = 'black';
   box.collider = 'static';
   box.bounciness = 0.0;
 
@@ -54,8 +56,8 @@ function setup() {
   new box.Sprite(width/2, height-wallthick/2, width, wallthick);
 
   dividers = new Group();
-  dividers.color = '#ffcc99';
-  dividers.stroke = '#ffcc99';
+  dividers.color = '#000000';
+  dividers.stroke = '#000000';
   dividers.collider = 'static';
   dividers.width = divthick;
   dividers.height = divheight;
@@ -67,16 +69,16 @@ function setup() {
     new dividers.Sprite(x, height-wallthick-divheight-divthick/2, divthick, 'triangle');
   });
 
-  pinpos = equalSpacing(pinmax-1, pinsize);
+  pinpos = equalSpacing(pinmax, pinsize);
   let pinxsep = pinpos[1] - pinpos[0];
   let pinysep = pinxsep * Math.sqrt(3)/2;
 
   pins = new Group();
-  pins.stroke = 'gray';
-  pins.color = 'white';
+  pins.stroke = 'black';
+  pins.color = 'black';
   pins.diameter = pinsize;
   pins.collider = 'static';
-  pins.bounciness = 0.05;
+  pins.bounciness = 0.15;
   let y = pintop;
   for (let r=pinstart; r <= pinrows+pinstart; r++) {
     y += pinysep;
@@ -89,20 +91,21 @@ function setup() {
 
   balls = new Group();
   balls.diameter = ballsize;
-  balls.color = 'green';
+  balls.color = () => ballcolors[ballcolorindex % ballcolors.length];
+  balls.stroke = () => ballcolors[ballcolorindex % ballcolors.length];
   balls.x = width/2;
-  balls.y = height/11;
+  balls.y = height/10;
   balls.mass = 3;
-  balls.velocity.y = () => random(-2, -4);
-  balls.velocity.x = () => random(-1.5, 1.5);
-  balls.bounciness = 0.4;
+  balls.velocity.y = () => random(-4, -6);
+  balls.velocity.x = () => random(-.5, .5);
+  balls.bounciness = 0.2;
 
   balls.collides(pins, hitpin);
 }
 
 function hitpin(ball, pin) {
-  pin.stroke = 'red';
-  pin.color = 'red';
+  pin.stroke = 'white';
+  pin.color = 'white';
 }
 
 function collect(player, gem) {
@@ -111,15 +114,26 @@ function collect(player, gem) {
 
 function draw() {
 	clear();
-  if (frameCount % 3 == 0 && balls.size() < ballcount) {
-    new balls.Sprite();
-  }
+  background('#444444');
+  if (balls.size() < ballcount && frameCount % 3 == 0) {
+      new balls.Sprite();
+      if (balls.size() % 120 == 0) {
+        ballcolorindex++;
+      }
+    }
 
   // if balls get stuck up high, give them a little push
   balls.forEach((ball) => {
     if (ball.y < (height - divheight - wallthick) && ball.velocity.y == 0) {
-      ball.velocity.y = random(0.1, 0.3);
+      ball.velocity.y = random(0.9, 1.3);
+      // ball.velocity.x = random(-0.3, 0.3);
     }
   });
 
+  if (kb.presses(' ')) {
+    ballcolorindex = int(random(0, ballcolors.length));
+    balls.removeAll();
+    pins.color = 'black';
+    pins.stroke = 'black';
+  }
 }
