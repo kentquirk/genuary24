4738a85aead5ca2c474eec1bd888cb875723dd93
Kent Quirk <kentquirk@honeycomb.io>
2023-12-31 15:26:36 -0500

    More responsive

--- p5/particles2/sketch.js
+++ p5/particles2/sketch.js
@@ -1,23 +1,72 @@
 let wall, flying, still;
 
-let width = 900;
-let height = 900;
-let wallwid = 40;
-let leftborder = 100;
-let diam = 50;
-var fallUntil = 0;
-var lastStuck = 0;
+let width;
+let height;
+let wallwid;
+let leftborder;
+let diam;
+var fallUntil;
+var lastStuck;
 
+function calcWidth(sz) {
+  if (sz < 600) {
+    return sz;
+  } else if (sz < 800) {
+    return 600;
+  } else if (sz < 1000) {
+    return 800;
+  } else if (sz < 1200) {
+    return 1000;
+  } else {
+    return 1200;
+  }
+}
 
-function setup() {
-	new Canvas(width, height);
-	world.gravity.y = 20;
+function windowResized() {
+  w = calcWidth(min(windowWidth, windowHeight));
+  if (w == width) {
+    return;
+  }
+  resizeCanvas(w, w);
+  init(w);
+}
 
-	wall = new Sprite(width-wallwid/2, height/2, wallwid, height, 'static');
+function setupWindow() {
+  w = calcWidth(min(windowWidth, windowHeight));
+  let canvas = createCanvas(w, w);
+  canvas.parent('sketch-holder');
+  init(w);
+}
+
+function init(sz) {
+  width = sz;
+  height = sz;
+  wallwid = sz/20;
+  leftborder = sz/10;
+  diam = sz/20;
+  fallUntil = 0;
+  lastStuck = frameCount;
+
+  world.gravity.y = sz/50;
+
+  if (wall) {
+    wall.remove();
+  }
+  wall = new Sprite(width-wallwid/2, height/2, wallwid, height, 'static');
+
+  if (!flying) {
+    flying = new Group();
+  }
+  flying.removeAll();
+
+  if (!still) {
+    still = new Group();
+  }
+  still.removeAll();
+
   wall.color = 'gray';
   wall.name = 'wall';
 
-  flying = new Group();
   flying.shape = 'circle';
   flying.x = 0;
   flying.y = () => random(150, height - 150);
@@ -26,9 +75,8 @@ function setup() {
   flying.diameter = diam;
   flying.color = () => color(random(128, 255), random(255), 0);
   flying.name = 'flying';
-  flying.cull(1000);
+  flying.cull(500);
 
-  still = new Group();
   still.diameter = diam;
   still.shape = 'circle';
   still.velocity.x = 0;
@@ -64,10 +112,15 @@ function collide(moving, fixed) {
   moving.remove();
 }
 
+function setup() {
+  setupWindow();
+}
+
 function draw() {
   clear();
   background(0);
 
+  // Throw a ball every 10 frames
   if (frameCount % 10 == 0 && frameCount > fallUntil) {
     let sp = new flying.Sprite();
     if (still.collider == 'dynamic') {
@@ -76,11 +129,13 @@ function draw() {
     }
   }
 
+  // If we've been stuck for a while, make the still balls fall
   if ((frameCount > fallUntil) && (frameCount - lastStuck) > 150) {
     wall.color = 'red';
     still.collider = 'dynamic';
     still.velocity.x = () => random(-4, 0);
     fallUntil = frameCount + 100;
+    init(width);
   }
 
 }
