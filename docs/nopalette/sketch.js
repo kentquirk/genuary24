1eb679d99d9e93c56036d43283e22192316d53a8
Kent Quirk <kentquirk@honeycomb.io>
2024-01-13 22:33:18 -0500

    Update nopalette docs

--- docs/nopalette/sketch.js
+++ docs/nopalette/sketch.js
@@ -3,17 +3,34 @@ let height = 900;
 let delay = 1;
 let faderate = 0.02;
 let paused = false;
-let strokelevel = 1;
-let strokelevels = [() => noStroke(), () => stroke('black'), () => stroke('gray'), () => stroke('white')];
+let strokelevels = {
+  'none': () => noStroke(),
+  'black': () => stroke('black'),
+  'gray': () => stroke('gray'),
+  'white': () => stroke('white')
+};
 let monochrome = false;
+let gui;
+let frame;
+let hash;
+
+// things the gui manipulates have to be `var` not `let`
+var strokelevel = 1;
+var maxdepth = 8;
+var continuous = true;
+var strokeweight = 1
+var strokes = ['black', 'gray', 'white', 'none'];
+var userSeed = '';
+
 
 let rectangles = [];
 
-function Rectangle(x, y, w, h, col, targetCol) {
+function Rectangle(x, y, w, h, depth, col, targetCol) {
   this.x = x;
   this.y = y;
   this.w = w;
   this.h = h;
+  this.depth = depth;
   this.col = col;
   this.targetCol = targetCol || col;
 
@@ -24,15 +41,76 @@ function Rectangle(x, y, w, h, col, targetCol) {
     fill(this.col);
     rect(this.x, this.y, this.w, this.h);
   }
+
+  this.drawBorder = function() {
+    noFill();
+    rect(this.x+strokeweight/2, this.y+strokeweight/2, this.w-strokeweight, this.h-strokeweight);
+  }
+}
+
+/**
+ * Returns a hash code from a string
+ * @param  {String} str The string to hash.
+ * @return {Number}    A 24bit positive integer
+ * @see http://werxltd.com/wp/2010/05/13/javascript-implementation-of-javas-string-hashcode-method/
+ */
+function hashCode(str) {
+  let hash = 0;
+  for (let i = 0, len = str.length; i < len; i++) {
+      let chr = str.charCodeAt(i);
+      hash = (hash << 5) - hash + chr;
+      // fold it to 24 bits
+      hash = ((hash >> 8) ^ hash) & 0xffffff;
+  }
+  return hash;
 }
 
 function restart() {
+  if (userSeed) {
+    hash = hashCode(userSeed);
+    continuous = false;
+  } else {
+    let now = 'colorgrid'+day()+month()+year()+hour()+minute()+second()+millis();
+    hash = hashCode(now);
+  }
+  let elt = select('.seed');
+  if (elt) {
+    elt.html(hash);
+  }
+
+  randomSeed(hash);
+  paused = false;
+  width = windowWidth;
+  height = windowHeight;
   rectangles = [];
-  rectangles.push(new Rectangle(0, 0, width, height, randomColor()));
+  frame = new Rectangle(0, 0, width, height, 0, randomColor());
+  rectangles.push(frame);
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  restart();
+}
+
+function saveImage() {
+  saveCanvas(canvas, 'colorgrid_'+hash, 'png');
 }
 
 function setup() {
-	new Canvas(width, height);
+  canvas = createCanvas(windowWidth, windowHeight);
+  canvas.parent('sketch-holder');
+
+  gui = createGui("controls", this);
+  gui.setPosition(windowWidth-300, 50);
+  sliderRange(3, 15, 1);
+  gui.addGlobals("maxdepth");
+  gui.addGlobals("strokes", "continuous");
+  sliderRange(1, 10, 1);
+  gui.addGlobals("strokeweight");
+  gui.addGlobals('userSeed');
+  gui.addButton("restart", restart);
+  gui.addButton("save", saveImage);
+
   restart();
 }
 
@@ -69,18 +147,19 @@ function randomColor() {
 function draw() {
   clear();
   background(0);
-  strokelevels[strokelevel % strokelevels.length]();
+  strokeWeight(strokeweight);
+  strokelevels[strokes]();
 
   if (kb.presses(' ')) {
     paused = !paused;
+    if (continuous) {
+      restart();
+      return;
+    }
   }
 
   if (kb.presses('c')) {
     restart();
-  }
-
-  if (kb.presses('l')) {
-    strokelevel++;
   }
 
   if (kb.presses('s')) {
@@ -104,9 +183,15 @@ function draw() {
   if (frameCount % delay == 0 && !paused) {
     tries = 0;
     let ix = int(random(rectangles.length));
-    while (rectangles[ix].w < 20 || rectangles[ix].h < 20) {
+    while (rectangles[ix].depth >= maxdepth) {
+    // while (rectangles[ix].w < 20 || rectangles[ix].h < 20) {
       if (tries++ > 5) {
+        if (!continuous) {
+          paused = true;
+          return;
+        }
         restart();
+        return;
       }
       ix = int(random(rectangles.length));
     }
@@ -130,12 +215,12 @@ function draw() {
     }
     if (splith) {
       let newh = int(h*splitPos);
-      newRectangles.push(new Rectangle(x, y, w, newh, r.col, c1));
-      newRectangles.push(new Rectangle(x, y+newh, w, h-newh, r.col, c2));
+      newRectangles.push(new Rectangle(x, y, w, newh, r.depth+1, r.col, c1));
+      newRectangles.push(new Rectangle(x, y+newh, w, h-newh, r.depth+1, r.col, c2));
     } else {
       let neww = int(w*splitPos);
-      newRectangles.push(new Rectangle(x, y, neww, h, r.col, c1));
-      newRectangles.push(new Rectangle(x+neww, y, w-neww, h, r.col, c2));
+      newRectangles.push(new Rectangle(x, y, neww, h, r.depth+1, r.col, c1));
+      newRectangles.push(new Rectangle(x+neww, y, w-neww, h, r.depth+1, r.col, c2));
     }
 
     rectangles.splice(ix, 1);
@@ -144,5 +229,10 @@ function draw() {
 
   for (let r of rectangles) {
     r.draw();
+  }
+  frame.drawBorder();
+
+  if (kb.presses('!')) {
+    saveImage();
   }
 }
