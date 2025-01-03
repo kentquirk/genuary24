6c70b1f4905b124b41b74b006bb09f62f74e0fea
Kent Quirk <kentquirk@honeycomb.io>
2025-01-03 18:01:27 -0500

    better

--- p5/fortytwo/sketch.js
+++ p5/fortytwo/sketch.js
@@ -1,42 +1,42 @@
-let tf, bgcolor = 0;
+let tfs, bgcolor = 0;
 function setup() {
   let canvas = createCanvas(windowWidth, windowHeight);
   canvas.parent('sketch-holder');
-  tf = makeTF();
+  tfs = makeTFs();
 }
 
-function makeTF() {
-  let sc = random(0.9, 1.1);
-  let rt = random(-10, 10);
-  let trX = random(-20, 20);
-  let trY = random(-20, 20);
-  let tf = {
-    scale: (n) => { scale(Math.pow(sc, n)); },
-    rotate: (n) => { rotate(rt*n); },
-    translate: (n) => { translate(trX*n, trY*n); }
-  };
-  return tf;
+function makeTFs() {
+  tfs = [];
+  for (let n = random(5, 15); n > 0; n--) {
+    switch (int(random(3))) {
+      case 0:
+        let sc = random(0.9, 1.1);
+        tfs.push((n) => scale(Math.pow(sc, n)));
+        break;
+      case 1:
+        let rt = random(-20, 20);
+        tfs.push((n) => rotate(rt*n));
+        break;
+      case 2:
+        let tr = createVector(random(-40, 40), random(-40, 40));
+        tfs.push((n) => translate(tr));
+        break;
+    }
+  }
+  return tfs;
 }
 
 function draw() {
   background(bgcolor);
-  let maxN = 50;
-  let maxT = 100;
-  if (frameCount % (maxT*3) === 0) {
+  if (frameCount % (300) === 0) {
     bgcolor = color(random(255), random(255), random(255));
-    tf = makeTF();
+    tfs = makeTFs();
   }
   translate(width / 2, height / 2);
-  fill(255);
-  stroke(0);
-  textSize(172);
+  fill(255); stroke(0); textSize(172);
   push();
-  let n = maxN/2 + (maxN/2 * sin(360 * frameCount / maxT));
-  for (let typ of ["rotate", "translate", "scale"]) {  // have to do these in order
-    if (tf[typ]) {
-        tf[typ](n);
-    }
-  }
+  let n = 25 + (25 * sin(360 * frameCount / 100));
+  for (let tf of tfs) { tf(n); }
   text("42", -110, 70);
   pop();
 }
